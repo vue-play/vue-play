f40e862828eaef6311183f9b195870c557174eb1
EGOIST <0x142857@gmail.com>
2016-10-08 14:53:20 +0800

    fix main width

--- src/components/App.vue
+++ src/components/App.vue
@@ -25,10 +25,14 @@
     margin: 0;
     font: 14px -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
   }
+  * {
+    box-sizing: border-box;
+  }
   .app {
     display: flex;
   }
   .main {
     padding: 10px;
+    width: calc(100% - 280px);
   }
 </style>
