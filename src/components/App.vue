c5d8947d12118a984a72cca1784abc96e81beb29
EGOIST <0x142857@gmail.com>
2016-10-11 23:32:29 +0800

    hide body overflow

--- src/components/App.vue
+++ src/components/App.vue
@@ -25,6 +25,7 @@
   body {
     margin: 0;
     font: 14px -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+    overflow: hidden;
   }
   * {
     box-sizing: border-box;
