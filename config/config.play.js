dfe353f708fff813ad799452c7a20c3f693c5beb
EGOIST <0x142857@gmail.com>
2017-05-21 09:44:25 +0800

    use keyword to add hmr entry

--- config/config.play.js
+++ config/config.play.js
@@ -1,18 +1,14 @@
 const path = require('path')
 const config = require('./')
-const hmrClientDir = path.join(__dirname, '../node_modules/poi/app/dev-client.es6')
 
 module.exports = {
   entry: {
-    app: './play/app.js',
-    preview: './play/preview.js'
+    app: [':hot:', './play/app.js'],
+    preview: [':hot:', './play/preview.js']
   },
   dist: 'dist-play',
   postcss: config.postcss,
   webpack (config) {
-    ['app', 'preview'].forEach(entry => {
-      config.entry[entry].unshift(hmrClientDir)
-    })
     config.resolve.modules.push(path.resolve('src'))
     return config
   },
